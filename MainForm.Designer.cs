cdbf7fe9030fb38ec77c7b30fd9f6c7bead60231
LazarBojanic <lbojanic11621rn@raf.rs>
2022-08-02 04:56:58 +0200

    misc

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -54,6 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.buttonPrikazUgovora);
             this.Controls.Add(this.buttonPrikazOpstina);
