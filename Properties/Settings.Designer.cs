2b15920782c5b167dfa33b805162e06efb641514
LazarBojanic <bojanic.lazar50@gmail.com>
2023-05-14 06:22:58 +0200

    misc

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace InfoplanContractManagmentSoftware.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\\\..\\\\..\\\\ugovori.accdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=.\\\\ugovori.accdb")]
         public string ugovoriConnectionString {
             get {
                 return ((string)(this["ugovoriConnectionString"]));
